12805ae0888539668a5e7c873ee69706f49d8631
Yanters <yanter@wp.pl>
2022-12-26 20:13:49 +0100

    Displaying expenses

--- components/Expenses/ExpensesSummary.js
+++ components/Expenses/ExpensesSummary.js
@@ -1,4 +1,5 @@
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { GlobalStyles } from '../../constants/styles';
 
 const ExpensesSummary = ({ expenses, periodName }) => {
   const expensesSum = expenses.reduce((sum, expense) => {
@@ -6,11 +7,31 @@ const ExpensesSummary = ({ expenses, periodName }) => {
   }, 0);
 
   return (
-    <View>
-      <Text>{periodName}</Text>
-      <Text>${expensesSum.toFixed(2)}</Text>
+    <View style={styles.container}>
+      <Text style={styles.period}>{periodName}</Text>
+      <Text style={styles.sum}>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 };
 
 export default ExpensesSummary;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  period: {
+    fontSize: 16,
+    color: GlobalStyles.colors.primary400,
+  },
+  sum: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: GlobalStyles.colors.primary500,
+  },
+});
---
+++ util/date.js
@@ -0,0 +1,6 @@
+export const getFormattedDate = (date) => {
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
+};
